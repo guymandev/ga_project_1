d74b50b8be07f4dfe082279ac6a9f35b8149a083
Guy Jara <guy_jara@yahoo.com>
2023-09-05 15:22:49 -0700

    Successfully initialized js variable to element using getElementbyId

---
+++ script.js
@@ -0,0 +1,4 @@
+const homeMenu = document.getElementById('home');
+// const homeMenu = document.querySelector('#home');
+// console.log('Testing initializing home menu object...');
+// console.log(homeMenu);
