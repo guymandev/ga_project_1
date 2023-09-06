363cc6a7ed3c01f2304f78b201e8165f8b547da3
Guy Jara <guy_jara@yahoo.com>
2023-09-06 09:57:34 -0700

    Got clicked menu name printing to console

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-const homeMenu = document.getElementById('home');
-// const homeMenu = document.querySelector('#home');
-// console.log('Testing initializing home menu object...');
-// console.log(homeMenu);
+const homeSection = document.getElementById('home');
+const aboutSection = document.getElementById('about');
+const resumeSection = document.getElementById('resume');
+const projectsSection = document.getElementById('projects');
+const linksSection = document.getElementById('links');
+
+const navMenu = document.getElementById('menu');
+const menuLinks = document.querySelectorAll('#menu a');
+
+// const menuList = ['home','about','resume','projects','links'];
+
+/* menuLinks.forEach(function(link) {
+    console.log(link.innerText);
+}); */
+
+// console.log(homeSection.id);
+
+navMenu.addEventListener('click', function(event) {
+    event.preventDefault();
+    const link = event.target;
+    if (link.tagName !== 'A') return;
+
+    console.log(link.innerText);
+    console.log(link.innerText.toLowerCase());    
+    // console.log(link.tagName);
+
+    /* menuLinks.forEach(function(link) {
+        if (link.innerText.toLowerCase() === )
+
+    }); */
+
+});
