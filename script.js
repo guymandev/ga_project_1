dc50d6bca16f6f24f5e13f0cb98ca72f15d34fb2
Guy Jara <guy_jara@yahoo.com>
2023-09-06 10:50:57 -0700

    Successfully referenced page section elements

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const homeSection = document.getElementById('home');
-const aboutSection = document.getElementById('about');
-const resumeSection = document.getElementById('resume');
-const projectsSection = document.getElementById('projects');
-const linksSection = document.getElementById('links');
+// const homeSection = document.getElementById('home');
+// const aboutSection = document.getElementById('about');
+// const resumeSection = document.getElementById('resume');
+// const projectsSection = document.getElementById('projects');
+// const linksSection = document.getElementById('links');
 
 const navMenu = document.getElementById('menu');
 const menuLinks = document.querySelectorAll('#menu a');
+const allSections = document.querySelectorAll('#main section');
 
 // const menuList = ['home','about','resume','projects','links'];
 
@@ -15,18 +16,28 @@ const menuLinks = document.querySelectorAll('#menu a');
 
 // console.log(homeSection.id);
 
+allSections.forEach(function(section) {
+    console.log(section.id);
+});
+
 navMenu.addEventListener('click', function(event) {
     event.preventDefault();
-    const link = event.target;
-    if (link.tagName !== 'A') return;
+    const clickedLink = event.target;
+    if (clickedLink.tagName !== 'A') return;
 
-    console.log(link.innerText);
-    console.log(link.innerText.toLowerCase());    
-    // console.log(link.tagName);
+    // console.log(link.innerText);
+    // console.log(clickedLink.innerText.toLowerCase());
+    
+    if (clickedLink.innerText.toLowerCase() !== 'contact') {
+        const docSection = document.getElementById(clickedLink.innerText.toLowerCase());
+        // console.log(docSection.id);
+    };
 
-    /* menuLinks.forEach(function(link) {
-        if (link.innerText.toLowerCase() === )
 
-    }); */
+
+    menuLinks.forEach(function(link) {
+        // if (link.innerText.toLowerCase() === )
+
+    });
 
 });
