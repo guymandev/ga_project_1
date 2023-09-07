b2aaf2f1aa650bb2831e12f8e8c17bf8082c31f9
Guy Jara <guy_jara@yahoo.com>
2023-09-07 14:48:09 -0700

    Added basic modal form, but input box is misaligned

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// const homeSection = document.getElementById('home');
-// const aboutSection = document.getElementById('about');
-// const resumeSection = document.getElementById('resume');
-// const projectsSection = document.getElementById('projects');
-// const linksSection = document.getElementById('links');
-
 const navMenu = document.getElementById('menu');
 const menuLinks = document.querySelectorAll('#menu a');
 const allSections = document.querySelectorAll('#main section');
-
-// const menuList = ['home','about','resume','projects','links'];
-
-/* menuLinks.forEach(function(link) {
-    console.log(link.innerText);
-}); */
-
-// console.log(homeSection.id);
-
-/* allSections.forEach(function(section) {
-    console.log(section.id);
-}); */
 
 navMenu.addEventListener('click', function(event) {
     event.preventDefault();
     const clickedLink = event.target;
     if (clickedLink.tagName !== 'A') return;
-
-    // console.log(link.innerText);
-    // console.log(clickedLink.innerText.toLowerCase());
     
     if (clickedLink.innerText.toLowerCase() !== 'contact') {
 
@@ -36,18 +15,34 @@ navMenu.addEventListener('click', function(event) {
             } else {
                 section.classList.add('hidden');
             };
-            // console.log(section.id);
-        });
-
-        // const docSection = document.getElementById(clickedLink.innerText.toLowerCase());
-        // console.log(docSection.id);
+        });       
     };
-
-
-
-    menuLinks.forEach(function(link) {
-        // if (link.innerText.toLowerCase() === )
-
-    });
-
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+    
+
